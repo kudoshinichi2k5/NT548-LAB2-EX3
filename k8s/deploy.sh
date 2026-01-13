a1676ef2b3734d71b6d18ce03c673526c7e5be57
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-01-13 16:04:07 +0700

    feat(deploy): update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -2,5 +2,6 @@
 kubectl apply -f namespace.yaml
 kubectl apply -f secret-user.yaml
 kubectl apply -f secret-recipe.yaml
+kubectl apply -f fe.yaml
 kubectl apply -f user-service-all.yaml
 kubectl apply -f recipe-service-all.yaml
